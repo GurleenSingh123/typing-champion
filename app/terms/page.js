9820301b001d51fd746641ea3e87fb513ec7668c
Gurleen Singh <gurleen@gmail.com>
2026-07-17 00:09:56 +0530

    file updated

---
+++ app/terms/page.js
@@ -0,0 +1,8 @@
+export default function Terms() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Terms & Conditions</h1>
+      <p>This page is under construction.</p>
+    </main>
+  );
+}
