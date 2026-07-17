f712ca30b1415def7d46e7715e8671e20a8fed9b
Gurleen Singh <gurleen@gmail.com>
2026-07-18 00:23:28 +0530

    file updated

---
+++ app/robots.js
@@ -0,0 +1,11 @@
+function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://typingchampion.com/sitemap.xml",
+  };
+}
+
+export default robots
