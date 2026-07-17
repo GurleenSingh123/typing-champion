c3fdba2a81e8fb23f8641045e10ae87b2f3a738d
Gurleen Singh <gurleen@gmail.com>
2026-07-18 00:09:18 +0530

    file updated

---
+++ app/sitemap.js
@@ -0,0 +1,25 @@
+function sitemap() {
+
+  return [
+    {
+      url: "https://typingchampion.com",
+    },
+    {
+      url: "https://typingchampion.com/about",
+    },
+    {
+      url: "https://typingchampion.com/contact",
+    },
+    {
+      url: "https://typingchampion.com/privacypolicy",
+    },
+    {
+      url: "https://typingchampion.com/result",
+    },
+    {
+      url: "https://typingchampion.com/terms",
+    },
+  ];
+}
+
+export default sitemap
