5323ec83dfa94f6ebae0643606085ebf57b951d4
Gurleen Singh <gurleen@gmail.com>
2026-07-19 16:40:13 +0530

    file updated

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,9 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-        <script src="https://quge5.com/88/tag.min.js" data-zone="261392" async data-cfasync="false"></script>
+        {/* <script src="https://quge5.com/88/tag.min.js" data-zone="261392" async data-cfasync="false"></script> */}
+
+        <Script src="https://quge5.com/88/tag.min.js" strategy="afterInteractive" data-zone="261392" data-cfasync="false"/>ṇ
 
         <meta name="monetag" content="00ed739fa4b175dd11749637bf1ae5df"></meta>
 
